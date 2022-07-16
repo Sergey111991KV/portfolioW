a59c260bd73f7b1e672bc942bcc15f311bfd3e24
Sergey <sergey.cosilov@gmail.com>
2022-07-16 11:46:24 +0300

    optimizations

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import Post from './post'
+import Post from '@models/post'
 import './styles/style.css'
-import WebpackTestPng from './assets/png-test.png'
-
-import json from './assets/json.json'
+import WebpackTestPng from './assets/png-test'
+import packages from '../package'
+import xml from './assets/test.xml'
+import csv from './assets/addresses.csv'
+import * as $ from 'jquery'
 
 const post = new Post('FIRST POST', WebpackTestPng)
 
 console.log(post.toStringW())
+console.log('package: ', packages)
+console.log('xml: ', xml)
+console.log('csv: ', csv)
 
-console.log('JSON', json)
+function showPackages() {
+    document.getElementById("json").innerHTML = JSON.stringify(packages, undefined, 2);
+}
+
+showPackages()
+
+$('#post').html(post.toStringW())
--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,36 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
+    context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: {
-        main: './src/index.js',
-        analytics: './src/analytics.js',
+        main: './index.js',
+        analytics: './analytics.js',
     },
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true
     },
+    resolve: {
+        extensions: ['.js', '.json', '.png'],
+        alias: {
+            '@models': path.resolve(__dirname, 'src/models'),
+            '@': path.resolve(__dirname, 'src')
+        }
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+    devServer: {
+        port: 4200
+    },
     plugins: [
         new HtmlWebpackPlugin({
-            title: "Webpack App",
-            template: "./src/index.html"
+            title: "Webpack App2",
+            template: "./index.html"
         }),
         new CleanWebpackPlugin()
     ],
@@ -27,8 +43,16 @@ module.exports = {
                 use: ['style-loader', 'css-loader']
             },
             {
-                test: /\.(png|svg|jpeg)$/,
-                use: ['file-loader']
+                test: /\.(png|svg|jpeg|jpg)$/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(xml)$/,
+                use: ['xml-loader']
+            },
+            {
+                test: /\.(csv)$/,
+                use: ['csv-loader']
             }
         ]
     }
